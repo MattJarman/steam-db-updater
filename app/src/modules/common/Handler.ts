492c8b48d449abf018d10e61ea4328ea678ca590
Matthew <mattjarman22@outlook.com>
2020-12-22 14:24:09 +0000

    Remove env files, Remove unused packaged

--- app/src/modules/common/Handler.ts
+++ app/src/modules/common/Handler.ts
@@ -1,8 +1,6 @@
 import Updater from './Updater'
-import * as dotenv from 'dotenv'
 
 export const handler = async (): Promise<boolean> => {
-  dotenv.config()
   const updater = new Updater()
   await updater.run()
   return true
